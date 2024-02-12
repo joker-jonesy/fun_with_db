f101451727ae31bb33ac785c8ca6fefa93ece59e
joker-jonesy <spashmcgrath94@yahoo.com>
2024-02-12 12:05:26 -0500

    kermit

--- api/index.js
+++ api/index.js
@@ -1,11 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-
-router.get('/location', (req, res)=>{
-    res.send("Denmark")
-})
-
 router.use("/sets", require("./sets"))
 router.use("/tags", require("./tags"))
 
